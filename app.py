db7f3a59096309395b617f301d8592e717431e97
DenisMurynka <dmurinka@gmail.com>
2021-02-26 22:48:41 +0200

    altered tables columns

--- app.py
+++ app.py
@@ -19,8 +19,11 @@ class Articles(db.Model):
         id = db.Column(db.Integer,primary_key = True)
         title = db.Column(db.String(100),nullable = False)
         intro = db.Column(db.String(300),nullable = False)
+        likes = db.Column(db.Integer)
+        dislikes = db.Column(db.Integer)
         text = db.Column(db.Text,nullable = False)
         date = db.Column(db.DateTime, default = datetime.utcnow)
+
 
         def __repr__(self):
             return '<Articles %r>' % self.id
