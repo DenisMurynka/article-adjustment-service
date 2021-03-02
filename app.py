9aed5bc92847266bc193a4fcec7fda9fce744a93
DenisMurynka <dmurinka@gmail.com>
2021-03-02 10:10:53 +0200

    fixed date parsing

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
 from flask import Flask,render_template,url_for,request,redirect,flash,jsonify
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy import  and_
+import json
+import  sqlalchemy
+import sqlite3
+import flask_login
 from flask_login import UserMixin,LoginManager,login_user,logout_user,login_required
 from werkzeug.security import check_password_hash,generate_password_hash
 from datetime import datetime as dt,timedelta
 import jwt  #specified version== 1.7.1
 from functools import wraps
-import sys
+import sys   #cant use COKIES for handling token  ,because of cant import flask_extended ...
+
 app = Flask(__name__)
-#app.secret_key='some secret salt'
-app.config['SECRET_KEY'] = 'some secret salt'
-app.config['JWT_LOCATION'] = ['COOKIES']
-app.config['JWT_COOKIE_SECURE'] = False
-app.config['JWT_REFRESH_COOKIE_PATH'] = '/token/refresh'
-app.config['JWT_ACCESS_COOKIE_PATH'] = '/api/'
-app.config['JWT_COOKIE_CSRF_PROTECT'] = True
+app.secret_key='some secret salt'
+#app.config['SECRET_KEY'] = 'some secret salt'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///articles_adjustment.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  #if you see notice 'SQLALCHEMY_TRACK_MODIFICATIONS'--> add this config,it is not important thought
 db = SQLAlchemy(app)
-manager = LoginManager(app)
-
+#manager = LoginManager(app)
 
 
 class Articles(db.Model):
@@ -29,11 +31,9 @@ class Articles(db.Model):
         intro = db.Column(db.String(300),nullable = False)
         likes = db.Column(db.Integer)
         dislikes = db.Column(db.Integer)
-        text = db.Column(db.Text,nullable = False)
-        date = db.Column(db.DateTime, default = dt.utcnow)
+        text = db.Column(db.Text,nullable=False)
+        date = db.Column(db.DateTime, default=dt.utcnow)
         user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
-
-
 
         def __repr__(self):
             return '<Articles %r>' % self.id
@@ -43,14 +43,25 @@ class User(db.Model,UserMixin):
     login = db.Column(db.String(32),nullable=False,unique=False)
     password = db.Column(db.String(255),nullable=False)
 
+class ActionEvent(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    article_id = db.Column(db.Integer, db.ForeignKey('articles.id'), nullable=False)
+    action = db.Column(db.String(32), nullable=False)
+    date = db.Column(db.DateTime, default=dt.utcnow)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id) #get_or_404?
 
 def token_required(f):
     @wraps(f)
     def decorated(*args,**kwargs):
-        #tmp=(sys.argv[1])
-        #print(tmp['token'].decode("utf-8") )
-        #token=tmp['token'].decode("utf-8")
+       # tmp=(sys.argv[0])
+       # print(tmp['token'].decode("utf-8") )
+        print(sys.argv)
+       # token=tmp['token'].decode("utf-8")
+
         token=((sys.argv[1])['token']).decode("utf-8")
         if not token:
             return jsonify({'message': 'Token is missing!'}), 403
@@ -61,11 +72,32 @@ def token_required(f):
         return f(*args,**kwargs)
     return decorated
 
+def parse_date(date):
+    return str(date).split(' ', 1)[0]
+
+@app.route('/analitics')# http://127.0.0.1:5000/analitics?dateFrom=2019-10-01&dateTo=2022-01-01
+def analytics_statist():
+
+    dateFrom = request.args.get('dateFrom')
+    #print(dateFrom)
+    dateTo = request.args.get('dateTo')
+    #print(dateTo)
+    result = db.session.execute("SELECT * from articles as A WHERE A.date >= :val and A.date <= :val2", {'val': dateFrom, 'val2': dateTo})
+
+    return jsonify({'result': [dict(row) for row in result]})
+
+
 @app.route('/like/<int:postId>')
 def like(postId):
-
    article = Articles.query.filter_by(id=postId).first()
+   data=jwt.decode((sys.argv[1])['token'],app.config['SECRET_KEY'])
+
+   actionEvent = ActionEvent(user_id=data['public_id'],
+                             article_id=article.id,
+                             action='like')
    article.likes += 1
+
+   db.session.add(actionEvent)
    db.session.commit()
    return redirect("/posts")
 
@@ -73,16 +105,21 @@ def like(postId):
 def dislike(postId):
 
    article = Articles.query.filter_by(id=postId).first()
+   data = jwt.decode((sys.argv[1])['token'], app.config['SECRET_KEY'])
+
+   actionEvent = ActionEvent(user_id=data['public_id'],
+                             article_id=article.id,
+                             action='dislike')
+
    article.likes -= 1
    article.dislikes += 1
+   db.session.add(actionEvent)
    db.session.commit()
    return redirect("/posts")
 
 
 
-@manager.user_loader
-def load_user(user_id):#id?
-    return User.query.get(user_id) #get_or_404?
+
 
 @app.route('/')
 @app.route('/home')
@@ -91,15 +128,14 @@ def index():
 
 
 @app.route('/about')
-#@login_required
+@login_required
 def about():
     return render_template('about.html')
 
 
 @app.route('/posts')
-@token_required
+@flask_login.login_required
 def posts():
-    #print('lolllll')
     articles = Articles.query.order_by(Articles.date.desc()).all()
     return render_template("posts.html", articles=articles)
 
@@ -123,16 +159,21 @@ def posts_delete(id):
 
 
 @app.route('/create-article', methods=['POST','GET'])
-@login_required
+@token_required
 def create_article():
+
     if request.method == "POST" and user:
         title = request.form['title']
         intro = request.form['intro']
         text = request.form['text']
+        data = jwt.decode((sys.argv[1])['token'], app.config['SECRET_KEY'])
+        #print('data: ' + str(data))
 
-        article = Articles(title = title,
-                           intro = intro,
-                           text  = text)
+
+        article = Articles(title=title,
+                           intro=intro,
+                           text=text,
+                           user_id=data['public_id'])
         try:
             db.session.add(article)
             db.session.commit()
@@ -150,9 +191,6 @@ def post_update(id):
         article.intro = request.form['intro']
         article.text = request.form['text']
 
-        # article = Articles(title = title,
-        #                    intro = intro,
-        #                    text  = text)
         try:
             #db.session.add(article)
             db.session.commit()
@@ -181,16 +219,16 @@ def login_page():
         user = User.query.filter_by(login=login).first()
 
         if  user and check_password_hash(user.password, password):
-            token = jwt.encode({'public_id': user.id, 'exp': dt.utcnow() + timedelta(seconds=10)}, app.config['SECRET_KEY'])
+            token = jwt.encode({'public_id': user.id, 'exp': dt.utcnow() + timedelta(minutes=10)}, app.config['SECRET_KEY'])
             sys.argv.append({'token': token})
-
+            print(token)
 
             #token = jwt.encode({'public_id':user.id,'exp':datetime.utcnow() + datetime.timedelta(minutes=30)},app.secret_key)
             login_user(user)
 
-            next_page = request.args.get('next')
+            #next_page = request.args.get('next')
 
-            return redirect(next_page)
+            return redirect(url_for('posts'))
             #return jsonify({'token': token.decode('UTF-8')})
         else:
             flash('Login or password is not correct')
